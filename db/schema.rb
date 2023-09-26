3c4d6da3aa6f251ad6ce6bd8e3acbd99119765c6
Carlo Poblete <cfcpoblete@gmail.com>
2023-09-26 12:45:26 +0800

    Add quantity migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_13_031437) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_26_044422) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -148,6 +148,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_13_031437) do
     t.integer "side_placket", default: 0, null: false
     t.string "fabric_consumption"
     t.string "back_placket"
+    t.integer "quantity", default: 0, null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -181,6 +182,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_13_031437) do
     t.integer "lapel_style", default: 0, null: false
     t.integer "adjuster_type", default: 0, null: false
     t.string "fabric_consumption"
+    t.integer "quantity", default: 0, null: false
   end
 
 end
