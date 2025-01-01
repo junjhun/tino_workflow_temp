94ee37619339270c70d21289a8b6ed59e15e90cf
Carlo Poblete <cfcpoblete@gmail.com>
2025-01-01 20:45:45 +0800

    Fix dashboard controller from pages controller

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,4 @@
-class PagesController < ApplicationController
+class DashboardController < ApplicationController
   def home
     # Fetch clients and group them by the date they were created
     @clients = Client.all.group_by { |client| client.created_at.to_date }
