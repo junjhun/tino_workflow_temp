a53374392557417038b59465403832632abe2d1a
Carlo Poblete <cfcpoblete@gmail.com>
2025-01-08 23:28:17 +0800

    Refix migrations for production to work

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,8 +5,10 @@ class Order < ApplicationRecord
 
     # validates :name, presence: true
     # validates :name, presence: true, uniqueness: true
-    
-    
+    validates :status, :client, :purpose, :brand_name, :type_of_service, :finish, :jo_number, presence: true
+    validates :jo_number, numericality: { only_integer: true, message: "must be a valid number" }, presence: true
+
+
     enum status: [
         "Client Appointment", 
         "JO's to receive by the PRODUCTION MANAGER (LAS PIÑAS)",
@@ -27,14 +29,14 @@ class Order < ApplicationRecord
         end
     end
 
-    enum purpose: [
-        "Bride",
-        "Groom",
-        "Wedding entourage or guest",
-        "Business",
-        "Personal Style",
-        "Travel",
-    ]
+    # enum purpose: [
+    #     "Bride",
+    #     "Groom",
+    #     "Wedding entourage or guest",
+    #     "Business",
+    #     "Personal Style",
+    #     "Travel",
+    # ]
  
     enum type_of_service: [
         "Bespoke",
