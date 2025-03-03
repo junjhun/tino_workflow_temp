0b7036e54cf9a779cd43a1b74a1d3189bfec5659
Mark Turingan <turingan.markn@gmail.com>
2025-03-03 14:58:41 +0800

    Remove migration to rename 'heard_from_source' column in clients table

--- db/migrate/20250228024423_rename_heard_from_source_to_heard_from.rb
+++
@@ -1,6 +0,0 @@
-class RenameHeardFromSourceToHeardFrom < ActiveRecord::Migration[7.0]
-  def change
-    rename_column :clients, :heard_from_source, :heard_from
-    change_column :clients, :heard_from, :integer, default: 4, null: false
-  end
-end
